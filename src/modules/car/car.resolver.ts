b79f076372a076b72f5b0ec0727e17c1b34a9891
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2024-10-23 15:46:33 +0500

    get one car query add

--- src/modules/car/car.resolver.ts
+++ src/modules/car/car.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, ID, Int, Query, Resolver } from '@nestjs/graphql';
 import { Car } from './models';
 import { CarService } from './car.service';
+import { ParseIntPipe } from '@nestjs/common';
 
 @Resolver(() => Car)
 export class CarResolver {
@@ -9,5 +10,10 @@ export class CarResolver {
   @Query(() => [Car])
   getCarlist() {
     return this.service.getCarList();
+  }
+
+  @Query(() => Car)
+  getOneCar(@Args('id', { type: () => Int }, ParseIntPipe) id: number) {
+    return this.service.getOneCar(id);
   }
 }
--- src/modules/car/car.service.ts
+++ src/modules/car/car.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Car } from './models';
 
 @Injectable()
 export class CarService {
+  cars: Car[] = [
+    { id: 1, brand: 'BMW', price: 200000, color: 'black', year: 2023 },
+  ];
+
   constructor() {}
 
   getCarList() {
-    return [{ id: 1, brand: 'BMW', price: 200000, color: 'black', year: 2023 }];
+    return this.cars;
+  }
+
+  getOneCar(id: number) {
+    return this.cars.find((c) => c.id == id);
   }
 }
