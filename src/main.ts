0193fbdf85ad0a38ea4cf4b55c431ab49458ae71
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2024-10-23 15:08:42 +0500

    graphql add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = app.get(ConfigService);
+
+  await app.listen(
+    config.get<number>('app.port'),
+    config.get<string>('app.host'),
+    (): void => {
+      console.log(`Listening on ${config.get<number>('app.port')}`);
+    },
+  );
 }
 bootstrap();
